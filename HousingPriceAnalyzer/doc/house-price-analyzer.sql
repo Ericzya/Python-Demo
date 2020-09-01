c92bed7708ea348524d333b2d513e3177e839f68
Eric <ericzya@outlook.com>
2020-09-01 17:06:12 +0800

    feat:Fix nonstandard column name

--- HousingPriceAnalyzer/doc/house-price-analyzer.sql
+++ HousingPriceAnalyzer/doc/house-price-analyzer.sql
@@ -20,14 +20,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `py_house_price`;
 CREATE TABLE `py_house_price` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `HouseCity` varchar(255) COLLATE utf8_bin NOT NULL,
-  `HouseArea` varchar(255) COLLATE utf8_bin NOT NULL,
-  `HouseCommunity` varchar(255) COLLATE utf8_bin NOT NULL,
-  `HousePrice` double(12,2) NOT NULL,
-  `HouseSquare` double(4,2) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `default` (`HouseCity`,`HouseArea`,`HouseCommunity`) USING BTREE
+  `id` int NOT NULL AUTO_INCREMENT,
+  `house_city` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `house_area` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `house_community` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `house_price` double(12,2) NOT NULL,
+  `house_square` double(4,2) NOT NULL,
+  `update_date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `default` (`house_city`,`house_area`,`house_community`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
