762fcbec58dbf1889ff9541d758957c08bd687f1
Eric <ericzya@outlook.com>
2020-09-01 17:50:02 +0800

    feat:Fix nonstandard column name

--- HousingPriceAnalyzer/models.py
+++ HousingPriceAnalyzer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class HousePrice(models.Model):
+    id = models.IntegerField(primary_key=True)
+    house_city = models.CharField(max_length=255)
+    house_area = models.CharField(max_length=255)
+    house_community = models.CharField(max_length=255)
+    house_price = models.BigIntegerField()
+    house_square = models.BigIntegerField()
+    update_date = models.DateTimeField('date published')
